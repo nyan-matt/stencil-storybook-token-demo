ffdfa2c8d740228003d8b378f11df75356df33e8
nyan-matt <matt.rea@gmail.com>
2023-02-06 15:55:35 +0000

    Deploying to main from @ nyan-matt/stencil-storybook-token-demo@6624f42efc4d694178c3ac23d22afd5f21e01e52 🚀

--- README.md
+++
@@ -1,8 +0,0 @@
-# About
-Part of proof-of-concept for design automation workflow using Figma tokens, Style Dictionary, Stencil, and Storybook. 
-
-# Stencil
-`npm start`
-
-# Storybook
-`npm run storybook`
---
+++ esm/loader.mjs
@@ -0,0 +1,1 @@
+import{b as bootstrapLazy}from"./index-82e155b0.js";import{a as patchEsm}from"./patch-075e5e5f.js";const defineCustomElements=(win,options)=>"undefined"==typeof window?Promise.resolve():patchEsm().then(()=>bootstrapLazy([["my-button",[[1,"my-button",{label:[1],size:[1],variant:[1],disabled:[4]}]]]],options));export{defineCustomElements};
