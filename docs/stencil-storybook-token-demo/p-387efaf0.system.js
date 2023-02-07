4d3b056c4224566be4d04e155223a6b075c82360
nyan-matt <matt.rea@gmail.com>
2023-02-07 01:34:37 +0000

    Deploying to main from @ nyan-matt/stencil-storybook-token-demo@d0b6f8885f8df0b0b92aae96a80bf9816480847e 🚀

--- docs/stencil-storybook-token-demo/p-387efaf0.system.js
+++
@@ -1,1 +0,0 @@
-System.register(["./p-35c1d9a9.system.js","./p-33bc8d24.system.js"],(function(){"use strict";var t,e;return{setters:[function(e){t=e.b},function(t){e=t.p}],execute:function(){e().then((function(e){return t([["p-a993b6a3.system",[[1,"my-button",{label:[1],size:[1],variant:[1],disabled:[4]}]]]],e)}))}}}));
