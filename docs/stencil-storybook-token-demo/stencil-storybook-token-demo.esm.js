ccf6d3c10305f4c40419d10f4bd29b03779145ab
nyan-matt <nyan-matt@users.noreply.github.com>
2023-03-05 00:08:00 +0000

    build storybook

--- docs/stencil-storybook-token-demo/stencil-storybook-token-demo.esm.js
+++ docs/stencil-storybook-token-demo/stencil-storybook-token-demo.esm.js
@@ -1,1 +1,1 @@
-import{b as a}from"./p-1cfe09be.js";import{p as e}from"./p-5a059910.js";e().then(e=>a([["p-602e2f69",[[1,"my-button",{label:[1],size:[1],variant:[1],disabled:[4]}]]]],e));
+import{b as a}from"./p-1cfe09be.js";import{p}from"./p-5a059910.js";p().then(p=>a([["p-445fc339",[[1,"my-button",{label:[1],size:[1],variant:[1],disabled:[4]}]]]],p));
