08f1169a159d54269340b801fbdeca7fc3dba2c0
nyan-matt <matt.rea@gmail.com>
2023-03-05 12:28:05 +0000

    Deploying to gh-pages from @ nyan-matt/stencil-storybook-token-demo@4b3256d1f11089d230483754339452136bbb4dcf 🚀

--- docs/stencil-storybook-token-demo/stencil-storybook-token-demo.esm.js
+++ docs/stencil-storybook-token-demo/stencil-storybook-token-demo.esm.js
@@ -1,1 +1,1 @@
-import{b as a}from"./p-1cfe09be.js";import{p}from"./p-5a059910.js";p().then(p=>a([["p-445fc339",[[1,"my-button",{label:[1],size:[1],variant:[1],disabled:[4]}]]]],p));
+import{b as a}from"./p-1cfe09be.js";import{p as b}from"./p-5a059910.js";b().then(b=>a([["p-1bfe3a69",[[1,"my-button",{label:[1],size:[1],variant:[1],disabled:[4]}]]]],b));
